b7ad034c2e21204292cdc216082d859a3df34d54
Strazdonis <edvinasstrazdonis@gmail.com>
2021-11-24 16:23:16 +0200

    add tests for API responses

---
+++ UnitTest/CryptoTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApiClient;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+
+    public interface ICryptoProvider
+    {
+        IList<CryptoModel> GetCryptos();
+    }
+
+    class CryptoTestProvider : ICryptoProvider
+    {
+        public IList<CryptoModel> Cryptos;
+        public IList<CryptoModel> GetCryptos()
+        {
+            return Cryptos;
+        }
+    }
+
+    [TestClass]
+    public class CryptoTests
+    {
+        [TestMethod]
+        public void TestCrypto()
+        {
+            var testProv = new CryptoTestProvider
+            {
+                Cryptos = new List<CryptoModel>
+                {
+                    new CryptoModel {
+                        asset_id = "BTC",
+                        name = "Bitcoin",
+                        type_is_crypto = "1",
+                        volume_1hrs_usd = "25901365694947.08",
+                        volume_1day_usd = "562462310296082.40",
+                        volume_1mth_usd = "18198075702757156.02",
+                        price_usd = "56128.210565066774379493699776"
+                    }
+                }
+            };
+
+            Assert.AreEqual("BTC", testProv.Cryptos[0].asset_id);
+            Assert.AreEqual("Bitcoin", testProv.Cryptos[0].name);
+            Assert.AreEqual("1", testProv.Cryptos[0].type_is_crypto);
+            Assert.AreEqual("25901365694947.08", testProv.Cryptos[0].volume_1hrs_usd);
+            Assert.AreEqual("562462310296082.40", testProv.Cryptos[0].volume_1day_usd);
+            Assert.AreEqual("18198075702757156.02", testProv.Cryptos[0].volume_1mth_usd);
+            Assert.AreEqual("56128.210565066774379493699776", testProv.Cryptos[0].price_usd);
+
+            Assert.AreEqual(testProv.GetCryptos(), testProv.Cryptos);
+        }
+    }
+}
---
+++ UnitTest/HistoryTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApiClient;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+
+
+    public interface IHistoryProvider
+    {
+        IList<HistoryModel> GetHistory();
+    }
+
+    class HistoryTestProvider : IHistoryProvider
+    {
+        public IList<HistoryModel> History;
+        public IList<HistoryModel> GetHistory()
+        {
+            return History;
+        }
+    }
+
+    [TestClass]
+    public class HistoryTests
+    {
+        [TestMethod]
+        public void TestHistory()
+        {
+
+            var testProv = new HistoryTestProvider
+            {
+                History = new List<HistoryModel>
+                {
+                    new HistoryModel {
+                        time_period_start = "2021-11-04T00:00:00.0000000Z",
+                        time_period_end = "2021-11-11T00:00:00.0000000Z",
+                        time_open = "2021-11-04T00:01:00.0000000Z",
+                        time_close = "2021-11-06T22:36:00.0000000Z",
+                        rate_open = "62947.46674573254",
+                        rate_high = "63113.76594975721",
+                        rate_low = "60136.677362254806",
+                        rate_close = "61301.7827063516"
+                    }
+                }
+            };
+
+            Assert.AreEqual("2021-11-04T00:00:00.0000000Z", testProv.History[0].time_period_start);
+            Assert.AreEqual("2021-11-11T00:00:00.0000000Z", testProv.History[0].time_period_end);
+            Assert.AreEqual("2021-11-04T00:01:00.0000000Z", testProv.History[0].time_open);
+            Assert.AreEqual("2021-11-06T22:36:00.0000000Z", testProv.History[0].time_close);
+            Assert.AreEqual("62947.46674573254", testProv.History[0].rate_open);
+            Assert.AreEqual("63113.76594975721", testProv.History[0].rate_high);
+            Assert.AreEqual("60136.677362254806", testProv.History[0].rate_low);
+            Assert.AreEqual("61301.7827063516", testProv.History[0].rate_close);
+
+            Assert.AreEqual(testProv.GetHistory(), testProv.History);
+        }
+    }
+}
